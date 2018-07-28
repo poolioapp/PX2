6b5080a29b7f8169f33b09f13a6f5ae2f31bd421
shichang hou <houshichang@hotmail.com>
2018-07-29 02:41:24 +1000

    added dummy files

---
+++ Poolio.Api/Queries/GetUserByEmailQuery.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Poolio.Api.Queries
+{
+    public class GetUserByEmailQuery
+    {
+    }
+}
